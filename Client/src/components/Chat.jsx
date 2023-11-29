55a6cd71e7c855af401d0d49fedf6a8146a9d2fd
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-11-29 09:27:28 -0500

    vamos a la API

--- Client/src/components/Chat.jsx
+++ Client/src/components/Chat.jsx
@@ -13,7 +13,7 @@ export function Chat () {
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:4040')
     setWs(ws)
-    ws.addEventListener('message', sendMessage)
+    ws.addEventListener('message', handleMessage)
   }, [])
 
   function showOnLinePeople (peopleArray) {
@@ -24,11 +24,21 @@ export function Chat () {
     setOnlinePeople(people)
   }
 
-  function sendMessage (e) {
+  function handleMessage (e) {
     const messageData = JSON.parse(e.data)
     if ('online' in messageData) {
       showOnLinePeople(messageData.online)
     }
+  }
+
+  function sendMessage (ev) {
+    ev.preventDefault()
+    ws.send(JSON.stringify({
+      message: {
+        recipient: selectedContact,
+        text: newMessageText
+      }
+    }))
   }
 
   const onlinePeopleExcluOurUser = { ...onlinePeople }
@@ -44,7 +54,7 @@ export function Chat () {
               <div className='w-1 bg-blue-500 h-12 rounded-r-md'></div>
             )}
             <div className='flex gap-2 py-2 pl-4 items-center'>
-              <Avatar username={onlinePeople[userId]} userId={userId}/>
+              <Avatar username={onlinePeople[userId]} userId={userId} />
               <span className='text-gray-800'>{onlinePeople[userId]}</span>
             </div>
           </div>
@@ -58,16 +68,18 @@ export function Chat () {
             </div>
           )}
         </div>
-        <form className="flex gap-2" onSubmit={}>
-          <input type="text" placeholder="Type Your Message" 
-            className="bg-white border p-2 flex-grow rounded-md" value={newMessageText} 
-            onChange={ev => setNewMessageText(ev.target.value)} />
-          <button className="bg-blue-500 p-2 text-white rounded-md">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-            </svg>
-          </button>
-        </form>
+        {!!selectedContact && (
+          <form className="flex gap-2" onSubmit={sendMessage}>
+            <input type="text" placeholder="Type Your Message"
+              className="bg-white border p-2 flex-grow rounded-md" value={newMessageText}
+              onChange={ev => setNewMessageText(ev.target.value)} />
+            <button type='submit' className="bg-blue-500 p-2 text-white rounded-md">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+              </svg>
+            </button>
+          </form>
+        )}
       </div>
     </section>
   )
