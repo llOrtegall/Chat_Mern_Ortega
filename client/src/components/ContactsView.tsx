c8085ea2b5579016c7e5701a334dcf0a0d9d8f02
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-21 16:27:41 -0500

    Refactor ContactsView component to display user email instead of username in online and offline people sections

--- client/src/components/ContactsView.tsx
+++ client/src/components/ContactsView.tsx
@@ -12,28 +12,28 @@ export const ContatsViews = ({ offlinePeople, onlinePeople, funSelectUserId, sel
   return (
     <section className='h-full'>
       {
-        onlinePeople.map(({ userId, username }) => (
+        onlinePeople.map(({ userId, email }) => (
           <section key={userId} onClick={() => funSelectUserId(userId)}
             className={'p-2 border border-b-2 dark:bg-slate-600 border-gray-300 dark:border-gray-600 flex items-center gap-2 cursor-pointer rounded-md mb-1' +
               (userId === selectUserId ? 'bg-blue-200 dark:bg-blue-700' : '')}>
             {
               userId === selectUserId && <span className='bg-blue-600 w-1 h-10 rounded-full'></span>
             }
-            <Avatar online={true} userId={userId} username={username} key={userId} />
-            <span className='text-gray-800 dark:text-white'>{username}</span>
+            <Avatar online={true} userId={userId} email={email} key={userId} />
+            <span className='text-gray-800 dark:text-white'>{email}</span>
           </section>
         ))
       }
       {
-        offlinePeople.map(({ _id, username }) => (
+        offlinePeople.map(({ _id, email }) => (
           <section key={_id} onClick={() => funSelectUserId(_id)}>
             <section className={'p-2 border border-b-2 dark:bg-slate-600 border-gray-300 dark:border-gray-600 flex items-center gap-2 cursor-pointer rounded-md mb-1' +
               (_id === selectUserId ? 'bg-blue-200 dark:bg-blue-700' : '')}>
               {
                 _id === selectUserId && <span className='bg-blue-600 w-1 h-10 rounded-full'></span>
               }
-              <Avatar online={false} userId={_id} username={username} key={_id} />
-              <span className='text-gray-800 dark:text-white'>{username}</span>
+              <Avatar online={false} userId={_id} email={email} key={_id} />
+              <span className='text-gray-800 dark:text-white'>{email}</span>
             </section>
           </section>
         ))
