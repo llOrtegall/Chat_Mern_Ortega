d070afe5fed6ed07750d120de10000e60fadedcd
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-21 16:17:04 -0500

    Refactor form width in LoginPage and RegisterPage

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -24,7 +24,7 @@ function LoginPage() {
 
   return (
     <section className='w-screen h-screen flex flex-col items-center justify-center pb-12'>
-      <form className='w-full px-64' onSubmit={handleSubmit}>
+      <form className='w-full max-w-96' onSubmit={handleSubmit}>
         <div className='relative z-0 w-full mb-5 group'>
           <input type='email' placeholder=' ' required value={email} onChange={e => setEmail(e.target.value)}
             className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer' />
