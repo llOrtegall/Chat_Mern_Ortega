706e58cb262981c9c06f404f6169e64c9210c9f8
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-20 21:45:58 -0500

    Refactor axios base URL configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import { UserContextProvider } from './context/UserContext'
+import { URL_API } from './utils/constans'
 import Routes from './routes/routes'
 import axios from 'axios'
 
 function App () {
-  axios.defaults.baseURL = 'http://192.168.1.6:4040'
+  axios.defaults.baseURL = `${URL_API}/`
   axios.defaults.withCredentials = true
 
   return (
---
+++ client/src/utils/constans.ts
@@ -0,0 +1,2 @@
+const URL_API = import.meta.env.VITE_URL_API;
+export { URL_API };
