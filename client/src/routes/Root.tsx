f0a95a3ad1e68d458c6ce2d63c1ebf9f964bb0ce
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-22 10:55:55 -0500

    Refactor Chat component to use email instead of username for displaying messages

--- client/src/routes/Root.tsx
+++ client/src/routes/Root.tsx
@@ -1,6 +1,6 @@
 import { useUser } from '../context/UserContext'
 import LoginPage from '../pages/LoginPage'
-import Chat from '../components/Chat'
+import Chat from '../pages/Chat'
 
 export default function Root () {
   const { id, email, } = useUser()
