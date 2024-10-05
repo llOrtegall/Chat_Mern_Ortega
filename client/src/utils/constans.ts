a942f5c9d5884eef4d694e6df608b4e4a545c2d4
llOrtegall <IvanOrtega_97@hotmail.com>
2024-10-05 09:44:38 -0500

    revisar el otro proyecto

--- client/src/utils/constans.ts
+++ client/src/utils/constans.ts
@@ -1,4 +1,4 @@
-const URL_API = import.meta.env.VITE_URL_API;
-const WS_API = import.meta.env.VITE_WS_API;
+const URL_API = import.meta.env.VITE_URL_API
+const WS_API = import.meta.env.VITE_WS_API
 
-export { URL_API, WS_API };
+export { URL_API, WS_API }
