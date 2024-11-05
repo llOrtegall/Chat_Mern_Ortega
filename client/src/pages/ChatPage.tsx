e3b63d81199eb93073b0e8513d9e097fb6af8c79
llOrtegall <IvanOrtega_97@hotmail.com>
2024-11-05 11:52:46 -0500

    refactor ChatPage button styles and add new icon for message input

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -209,7 +209,14 @@ export default function ChatPage() {
               <input value={newMsgText} onChange={(e) => setNewMsgText(e.target.value)}
                 type='text' placeholder='type your message here'
                 className='bg-white rounded-sm flex-grow px-1' />
-              <button className='bg-green-500 p-2 text-white rounded-sm' type='submit'>
+              <button 
+                className='bg-orange-500 hover:bg-orange-700 p-2 text-white rounded-sm' type='button' >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13" />
+                </svg>
+
+              </button>
+              <button className='bg-green-500 p-2 hover:bg-green-700 text-white rounded-sm' type='submit'>
                 <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='size-6'>
                   <path strokeLinecap='round' strokeLinejoin='round' d='M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5' />
                 </svg>
