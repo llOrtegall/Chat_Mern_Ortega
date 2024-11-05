f554793f24a605dd487cf9b935073c01cced5b5a
llOrtegall <IvanOrtega_97@hotmail.com>
2024-11-05 08:53:26 -0500

    refactor component avatar

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -5,7 +5,7 @@ export default function Avatar({ username, userId, online }: { username: string,
     'bg-purple-200', 'bg-pink-200'
   ]
 
-  const userIdBase10 = parseInt(userId, 16)
+  const userIdBase10 = parseInt(userId, 17)
   const colorIndex = userIdBase10 % colors.length
 
   const color = colors[colorIndex]
@@ -14,11 +14,7 @@ export default function Avatar({ username, userId, online }: { username: string,
   return (
     <div className={`w-10 h-10 ${color ? color : 'bg-white'} relative text-xl rounded-full flex items-center`}>
       <span className='w-full text-center opacity-60'>{username[0].toUpperCase()}</span>
-      {
-        online && (
-          <div className="absolute rounded-full w-3 h-3 bg-green-500 bottom-0 right-0 border shadow-md"></div>
-        )
-      }
+      <div className={`absolute rounded-full w-3 h-3 ${online ? 'bg-green-500' : 'bg-gray-500'} bottom-0 right-0 border shadow-md`}></div>
     </div>
   )
 }
