4886a84022a4a629cf3f1fa6c22a637e00cc5ed8
llOrtegall <IvanOrtega_97@hotmail.com>
2025-01-11 10:41:14 -0500

    feat: implement lazy loading for ChatPage and Register components with skeleton and loading indicators

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
-import { useUserContext } from './context/UserContext';
-import ChatPage from './pages/ChatPage';
-import Register from './pages/Register';
+import { SkeletonRegister } from '@/components/SkeletonRegister';
+import { useUserContext } from '@/context/UserContext';
+import { Suspense } from 'react';
+import { lazy } from 'react';
 import axios from 'axios';
+
+const ChatPage = lazy(() => import('@/pages/ChatPage'));
+const Register = lazy(() => import('@/pages/Register'));
 
 axios.defaults.baseURL = import.meta.env.VITE_URL_API;
 axios.defaults.withCredentials = true;
@@ -10,11 +14,17 @@ function App() {
   const { username, id } = useUserContext()
 
   if (username && id) {
-    return <ChatPage />
+    return (
+      <Suspense fallback={<div>loading ...</div>}>
+        <ChatPage />
+      </Suspense>
+    )
   }
 
   return (
-    <Register />
+    <Suspense fallback={<SkeletonRegister />}>
+      <Register />
+    </Suspense>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
-import { UserContextProvider } from './context/UserContext.tsx'
-import { ThemeProvider } from './context/ThemeProvider.tsx'
+import { UserContextProvider } from '@/context/UserContext.tsx'
+import { ThemeProvider } from '@/context/ThemeProvider.tsx'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from '@/App.tsx'
+import '@/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <UserContextProvider>
