8a5e38a622f0adfab06b19125ba224fe16d2058d
llOrtegall <IvanOrtega_97@hotmail.com>
2025-01-11 10:14:32 -0500

    feat: implement theme toggle functionality with dropdown menu

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
 import { UserContextProvider } from './context/UserContext.tsx'
+import { ThemeProvider } from './context/ThemeProvider.tsx'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ThemeProvider } from './context/ThemeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <UserContextProvider>
