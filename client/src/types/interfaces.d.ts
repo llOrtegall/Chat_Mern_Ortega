ada99cec095304bd6aadf369db3e58868d1548cc
llortegall <ivanortega_97@hotmail.com>
2025-05-17 01:21:22 +0000

    delete

--- client/src/types/interfaces.d.ts
+++
@@ -1,4 +0,0 @@
-export interface OnlinePeople {
-  userId: string
-  username: string
-}
--- server/src/routes/chat.routes.ts
+++
@@ -1,8 +0,0 @@
-import { getMessages, getPeople } from '@/controllers/chat.controller';
-import { Router } from 'express';
-
-export const chatRouter = Router();
-
-chatRouter.get('/messages/:userId', getMessages);
-
-chatRouter.get('/people', getPeople);
