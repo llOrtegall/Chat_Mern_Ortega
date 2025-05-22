05e0a9c1b56ca70d88708c41a6ca820e6e704d50
llOrtegall <IvanOrtega_97@hotmail.com>
2025-05-22 10:39:20 -0500

    firt init wss

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World");
+import WebSocket, { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 3010 });
+
+wss.on("connection", (ws) => {
+
+    ws.on("message", (message) => {
+        console.log(`Received message: ${message}`);
+    });
+
+    ws.on("close", () => {
+        console.log("Client disconnected");
+    });
+
+    ws.send("Hello from server");
+
+    [...wss.clients].forEach(c => console.log(c.readyState));
+
+});
