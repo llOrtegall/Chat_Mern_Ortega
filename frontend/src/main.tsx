fcf121eac219b43c78ad1d92c7e23671c5f96c5a
llortegall <ivanortega_97@hotmail.com>
2025-10-23 03:07:56 +0000

    add Tailwind CSS integration and update dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
+import { AuthProvider } from './context/auth/AuthContext.tsx'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { AuthProvider } from './context/auth/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
+    <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
     <App />
   </AuthProvider>
 )
